7008719ad3d6ef62755e924210e144bf97fc4836
vz415 <vdzaball@gmail.com>
2016-09-16 11:44:56 +0100

    linear regression between wins and points diff. How many points to score to win games?

--- NBApredictions.R
+++ NBApredictions.R
@@ -7,3 +7,5 @@ summary(NBA_train)
 # Tables for wins and playoffs
 table(NBA_train$W, NBA_train$Playoffs)
 NBA_train$PTSdiff <- NBA_train$PTS - NBA_train$oppPTS
+plot(NBA_train$PTSdiff, NBA_train$W)
+WinsReg <- lm(W~PTSdiff, data = NBA_train)
