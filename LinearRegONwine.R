ae3fdf171c4a8a21942d62ad75445ce44e788778
vz415 <vdzaball@gmail.com>
2016-09-16 10:48:29 +0100

    correlations.

--- LinearRegONwine.R
+++ LinearRegONwine.R
@@ -20,3 +20,7 @@ summary(model3)
 SSE3 <- sum(model3$residuals^2)
 SSE3
 # To understand which variables help the model predict wine price, look at the summary of the data, and see which variables have a ***, **, *, ., or ' ' by their name. Features that help predict the output will have asterisks
+# However, when you eliminate unnecessary features, there is a possibility that correlations come out of nowhere. Check with cor() function.
+cor(wine$WinterRain, wine$Price)
+cor(wine$Age, wine$FrancePop)
+# Because of multicollinearity, only want to remove one feature at a time to make sure you don't completely screw up the model
