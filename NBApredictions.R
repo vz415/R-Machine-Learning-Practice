602c1caf9523f1f6418eb309c35dce1245bac1ed
vz415 <vdzaball@gmail.com>
2016-09-16 11:34:48 +0100

    Made some tables, looking at difference in points.

--- NBApredictions.R
+++ NBApredictions.R
@@ -1,1 +1,9 @@
 # NBA Predictions
+NBA_train <- read.csv("NBA_train.csv")
+NBA_test <- read.csv("NBA_test.csv")
+# explore
+str(NBA_train)
+summary(NBA_train)
+# Tables for wins and playoffs
+table(NBA_train$W, NBA_train$Playoffs)
+NBA_train$PTSdiff <- NBA_train$PTS - NBA_train$oppPTS
